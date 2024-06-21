97ee5e090e9cae411bd0c8d2c8c921f0ff9fdb22
Luminaxis <luminaxistechnologies@gmail.com>
2024-06-21 16:32:44 +0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://cine-berry-api.vercel.app/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
